f6fe441b1c587f06fae485a01d1d2a2299ca5710
David Kim <innerb@gmail.com>
2020-04-20 13:18:08 -0700

    Using find().toArray to return tweets

--- routes/tweets.js
+++ routes/tweets.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const assert = require('assert');
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://atlasadmin:atlasadmin@cluster0-910s3.mongodb.net/anon?retryWrites=true&w=majority";
+const uri = "mongodb+srv://atlasadmin:atlasadmin@cluster0-910s3.mongodb.net/?retryWrites=true&w=majority";
+const dbName = 'anon';
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -10,16 +11,24 @@ const client = new MongoClient(uri, {
 let db;
 client.connect(err => {
     assert.equal(err, null);
-    db = client.db();
+    console.log("Connected successfully to server");
+    db = client.db(dbName);
 });
+const findDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection('tweets');
+    // Find some documents
+    collection.find({}).toArray( (err, docs) => {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(docs)
+        callback(docs);
+    });
+}
 router.get('/', function (req, res, next) {
-    const cursor = db.collection('tweets').find()
-    const resp = [];
-    cursor.forEach(doc => {
-        resp.push(doc.body_text);
-    }, err => {
-        res.send(resp.join('\n'));
-    })
+    findDocuments(db, docs => {
+        res.json(docs)
+    });
 });
 
 module.exports = router;
